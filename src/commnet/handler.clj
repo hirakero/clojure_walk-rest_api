0b6ecf065bb8681ba44d85c89183f8607e9fd2d3
kero_hirake <k16.h.white@gmail.com>
2022-02-02 11:11:05 +0900

    swagger 追加

--- src/commnet/handler.clj
+++ src/commnet/handler.clj
@@ -2,44 +2,72 @@
   (:require
    [reitit.core :as r]
    [reitit.ring :as ring]
+   [reitit.swagger :as swagger]
+   [reitit.swagger-ui :as swagger-ui]
+   [reitit.ring.middleware.muuntaja :as muuntaja]
+   [reitit.coercion.spec]
+   [reitit.ring.coercion :as coercion]
+   [reitit.ring.middleware.exception :as exception]
+   [muuntaja.core :as m]
    [ring.adapter.jetty :as jetty]))
 
-(def routes-reitit
-  [["/ping" {:lame ::ping}]
-   ["/pong" ::pong]
-   ["/api" {:a :1}
-    ["/users" ::users]
-    ["/users/:id" ::users-id]
-    ["/posts" ::posts]]])
+(def ok (constantly {:status 200 :body "ok"}))
 
-(def router-reitit
-  (r/router routes-reitit))
+(def routes
+  [["/swagger.json"
+    {:get {:no-doc true
+           :swagger {:info {:title "comment system API"
+                            :description "comment system API"}}
+           :handler (swagger/create-swagger-handler)}}]
+   
+   ["/comments"
+    {:swagger {:tags ["comments"]}}
 
-(clojure.pprint/pprint (r/match-by-path router-reitit "/ping"))
-(clojure.pprint/pprint (r/match-by-path router-reitit "/api/orders/1"))
-(clojure.pprint/pprint (r/match-by-name router-reitit ::ping))
+    [""
+     {:get {:summary "get all comments"
+            :handler ok}
 
-;;;;;;;;;;;;;;;;;;;;;;;;;
+      :post {:summary "create a new comment"
+             :parameters {:body {:name string?
+                                 :slug string?
+                                 :text string?
+                                 :parent-comment-id int?}}
+             :responses {200 {:body string?}}
+             :handler ok}}]
 
-(defn handler [_]
-  {:status 200, :body "ok"})
+    ["/:slug"
+     {:get {:summary "get comments by slug"
+            :parameters {:path {:slug string?}}
+            :handler ok}}]
 
-(def router 
-  (ring/router
-   ["/ping" {:get handler}]))
+    ["/id/:id"
+     {:get {:summary "get a comments by id"
+            :parameters {:path {:id int?}}
+            :handler ok}
 
-(clojure.pprint/pprint (r/match-by-path router "/pingping"))
+      :put {:summary "update a comment by the moderator"
+            :parameters {:path {:id int?}}
+            :handler ok}
 
-;;;;;;;;;;;;;;;;;;
+      :delete {:summary "delete a comment by the moderator"
+               :parameters {:path {:id int?}}
+               :handler ok}}]]])
 
-(def routes-ring
-  [["/ping" {:get (fn [req] {:status 200 :body "ok"})}]])
-
-(def router-ring
-  (ring/router routes-ring))
+(def router
+  (ring/router routes
+               {:data {:coercion reitit.coercion.spec/coercion
+                       :muuntaja m/instance
+                       :middleware [swagger/swagger-feature
+                                    muuntaja/format-middleware
+                                    exception/exception-middleware ;! coercion より先に
+                                    coercion/coerce-request-middleware
+                                    coercion/coerce-response-middleware]}}))
 
 (def app
-  (ring/ring-handler router-ring))
+  (ring/ring-handler router
+                     (ring/routes
+                      (swagger-ui/create-swagger-ui-handler
+                       {:path "/"}))))
 
 (defonce server (atom nil))
 (defn start []
@@ -51,7 +79,14 @@
     (.stop @server)
     (reset! server nil)))
 
+(defn restart []
+  (stop)
+  (start))
+
 (comment
- (app {:request-method :get :uri "/ping"})
- (start)
+  (app {:request-method :get :uri "/ping"})
+  (restart)
+
+  (start)
+  (stop)
   )
